9369d9e9c79d71ff42abde68076bc46c64d77f16
abbas <bmnswry27@gmail.com>
2024-04-30 13:20:23 +0330

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.utils import timezone
 from blog.models import Post
+from django.shortcuts import get_object_or_404
 def blog_view(request):
     posts = Post.objects.filter(status=1, published_date__lte=timezone.now())
     context = {'posts':posts}
     return render(request,'blog/blog-home.html',context)
 def blog_single(request, pid):
-    return render(request ,'blog/blog-single.html') 
+    post = get_object_or_404(
+        Post,
+        id=pid,
+        status=1,  
+        published_date__lte=timezone.now()
+    )
+
+    post.counted_view += 1
+    post.save()
+
+    context = {'post': post}
+    return render(request, 'blog/blog-single.html', context)
